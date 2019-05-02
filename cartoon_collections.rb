d0edc843b19eabc4b2f67759b601b5cd4bba6b3d
Travis Exline <t.exline22@gmail.com>
2019-05-02 17:31:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,7 +15,7 @@ def long_planeteer_calls(long_calls)
   end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(cheese_type)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_type.include?(cheese_types)
 end
