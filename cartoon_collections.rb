4999379d1ca348ba8faa0a91ce95f334e0109137
Travis Exline <t.exline22@gmail.com>
2019-05-02 17:20:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,9 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(long_calls)
-
+  long_calls.any? do |any_call|
+    any_call.length > 4
+  end
 end
 
 def find_the_cheese# code an argument here
